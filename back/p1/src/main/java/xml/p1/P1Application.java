ac1556d2efa4ed3db3dee10b27ce3c74077fc010
Lazar Pavlović <lp.pavlovic.001@gmail.com>
2022-12-10 22:21:48 +0100

    Dodat projekat za P1 obrazac i izgenerisane klase modela. Napravljeno ucitavanje iz fajla, ispis i izmena sadrzaja i upis u drugi fajl.

---
+++ back/p1/src/main/java/xml/p1/P1Application.java
@@ -0,0 +1,74 @@
+package xml.p1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.xml.sax.SAXException;
+import xml.p1.model.TFizickoLice;
+import xml.p1.model.ZahtevZaPriznanjePatenta;
+import xml.a1.util.MyValidationEventHandler;
+
+import javax.tools.FileObject;
+import javax.xml.XMLConstants;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+@SpringBootApplication
+public class P1Application {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(A1Application.class, args);
+		try {
+			System.out.println("[INFO] Example: JAXB unmarshalling/marshalling.\n");
+
+			// Definiše se JAXB kontekst (putanja do paketa sa JAXB bean-ovima)
+			JAXBContext context = JAXBContext.newInstance("xml.p1.model");
+
+			// Unmarshaller je objekat zadužen za konverziju iz XML-a u objektni model
+			Unmarshaller unmarshaller = context.createUnmarshaller();
+
+			// Marshaller je objekat zadužen za konverziju iz objektnog u XML model
+			Marshaller marshaller = context.createMarshaller();
+			// Podešavanje marshaller-a
+			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+
+			// XML schema validacija
+			SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+			Schema schema = schemaFactory.newSchema(new File("./data/p1schema.xsd"));
+
+			// Podešavanje unmarshaller-a za XML schema validaciju
+			unmarshaller.setSchema(schema);
+			unmarshaller.setEventHandler(new MyValidationEventHandler());
+
+			ZahtevZaPriznanjePatenta zahtev = (ZahtevZaPriznanjePatenta) unmarshaller.unmarshal(new File("./data/instance1.xml"));
+			marshaller.marshal(zahtev, System.out);
+
+			System.out.println("\n\n\n=============================================\n\n");
+
+			// Izmena nad objektnim modelom dodavanjem novog odseka
+			zahtev.getNazivPronalaska().setNazivPronalaskaNaSrpskom("*************************************** Novi neki naziv na srpskom ***************************************");
+			TFizickoLice liceNovo = (TFizickoLice) zahtev.getPodnosilacPrijave().getPodaciOLicu();
+			liceNovo.setIme("*************************************** Novo ime ***************************************");
+
+			// Umesto System.out-a, može se koristiti FileOutputStream
+			marshaller.marshal(zahtev, new FileOutputStream(new File("./data/p1Marshaller.xml")));
+			marshaller.marshal(zahtev, System.out);
+
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+		catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+		catch (SAXException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
---
+++ back/p1/src/test/java/xml/a1/A1ApplicationTests.java
@@ -0,0 +1,13 @@
+package xml.a1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
